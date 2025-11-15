5d082d9572e7c672562dd22589718548dfdb81ae
hemanthsampathi2008-creator <hemanthsampathi2008@gmail.com>
2025-11-15 13:48:30 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	char op;
+	double first , second;
+	printf("emter an operator(+,-,*,/):");
+	scanf("%c",&op);
+	printf("enter two operands:");
+	scanf("%lf %lf", &first, &second);
+	switch(op){
+	    case'+':
+	    printf("%.1lf+%.1lf=%.1lf",first,second,first+second);
+	    break;
+        case'-':
+	    printf("%.1lf-%.1lf=%.1lf",first,second,first-second);
+	    break;
+	    case'*':
+	    printf("%.1lf*%.1lf=%.1lf",first,second,first*second);
+	    break;
+	    case'/':
+	    printf("%.1lf/%.1lf=%.1lf",first,second,first/second);
+	    break;
+	    default:
+	    printf("error operator is not correct");
+    }
+
+return 0;
+
+}
